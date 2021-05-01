a2d03705e27021bffbdd2fa76c341f4a5fbc8918
Sathurian <77487450+Sathurian@users.noreply.github.com>
2021-05-01 13:36:30 +0530

    Add files via upload

---
+++ iron.js
@@ -0,0 +1,25 @@
+class Iron {
+    constructor(x, y) {
+      var options = {
+        'density':30,
+        'friction': 1.0,
+        'restitution':0.3
+      };
+      this.body = Bodies.rectangle(x, y, 100, 50, options);
+      this.width = 100;
+      this.height = 50;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+  
+      push();
+      translate(pos.x, pos.y);
+      strokeWeight(3);
+      stroke('black')
+      fill('brown')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
---
+++ stone.js
@@ -0,0 +1,25 @@
+class Stone {
+    constructor(x, y) {
+      var options = {
+        'density':10,
+        'friction': 1.0,
+        'restitution':0.3
+      };
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 50;
+      this.height = 50;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+  
+      push();
+      translate(pos.x, pos.y);
+      strokeWeight(3);
+      stroke('black')
+      fill('black')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
